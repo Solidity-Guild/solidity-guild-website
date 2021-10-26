1d1c934e2e1c4ef806f0323db26279810c590686
sfj.eth <0x0xsfj@gmail.com>
2021-10-25 20:03:59 -0700

    Redesign with Chakra UI
    New theme and style framework.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,68 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import '@fontsource/syne/700.css';
+import '@fontsource/syne/800.css';
+import '@fontsource/syne/variable.css';
+
+// Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: 'blue',
+    800: 'green',
+    700: 'red',
+  },
+  colors: {
+    transparent: 'transparent',
+    black: '#000',
+    white: '#FAE9D1',
+    gray: {
+      50: 'yellow',
+      900: 'orange',
+    },
+  },
+  fonts: {
+    heading: 'Syne',
+    body: 'Syne',
+  },
+};
+
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+};
+
+const theme = extendTheme({
+  fonts: {
+    heading: 'Syne',
+    body: 'Syne',
+  },
+  colors: {
+    transparent: 'transparent',
+    black: '#000',
+    white: '#FAE9D1',
+    whiteAlpha: {
+      900: '#FAE9D1',
+    },
+    gray: {
+      50: 'green',
+      700: 'purple',
+      800: '#151515',
+      900: 'orange',
+    },
+  },
+  config: {
+    initialColorMode: 'dark',
+    useSystemColorMode: false,
+  },
+});
+
+// const theme = extendTheme({ colors, config })
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
-
-export default MyApp
+export default MyApp;
